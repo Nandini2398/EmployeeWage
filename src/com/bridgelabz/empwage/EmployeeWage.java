a92d9a60bf4a4c0581c8fff07f6d5d47f5adfe9f
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-24 15:45:17 +0530

    UPDATED:using switch case statement

--- src/com/bridgelabz/empwage/EmployeeWage.java
+++ src/com/bridgelabz/empwage/EmployeeWage.java
@@ -9,14 +9,22 @@ public class EmployeeWage {
 	}
 }
 class EmpCalculation {
+	public static final int IS_FULLTIME = 1;
+	public static final int IS_PARTTIME = 2;
 	public static int empHrs;
 	public void empCheck() {
-		int empCheck = (int)Math.floor(Math.random() * 10) % 2;
-		if(empCheck == 1) {
-			System.out.println("Employee is present");
+		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+		switch(empCheck) {
+		case IS_FULLTIME:
+			System.out.println("Employee is present and worked full-time");
 			empHrs = 8;
-		}
-		else{
+			break;
+		case IS_PARTTIME:
+			System.out.println("Employee is present and worked part-time");
+			empHrs = 4;
+			break;
+			
+		default:
 			System.out.println("Employee is absent");
 		}
 	}
