d34537b3dc775e0d8818a67f21b44974532c4dfe
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-24 15:35:35 +0530

    UPDATED:employee is present or absent

--- src/com/bridgelabz/empwage/EmployeeWage.java
+++ src/com/bridgelabz/empwage/EmployeeWage.java
@@ -2,9 +2,25 @@ package com.bridgelabz.empwage;
 
 public class EmployeeWage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		public static void main(String[] args) {
+				Attendence attendence = new Attendence();
+				attendence.empCheck();
+			}
 	}
-
+	class Attendence {
+			public static final int IS_PRESENT = 1;
+			public static final int IS_ABSENT = 0;
+			public void empCheck() {
+				int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+				switch(empCheck) {
+				case IS_PRESENT: 
+					System.out.println("Employee is present");
+					break;
+				case IS_ABSENT:
+					System.out.println("Employee is absent");
+					break;
+				default:
+					System.out.println("Welcome");
+				}
+			}
 }
